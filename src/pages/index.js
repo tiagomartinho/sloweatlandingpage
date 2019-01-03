ef0cba12f711c560097e374477fc5dc961ea5d5d
Tiago Martinho <t.martinho@icloud.com>
2019-01-03 18:26:40 +0000

    Set title

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ class HomeIndex extends React.Component {
         return (
             <Layout>
                 <Helmet
-                    title="Gatsby Starter - Forty"
+                    title="Slow Eat"
                     meta={[
                         { name: 'description', content: 'Sample' },
                         { name: 'keywords', content: 'sample, something' },
