8a1031cd0f9dccc8df9c34a8b956f291ce94e683
Tiago Martinho <t.martinho@icloud.com>
2019-01-03 12:10:20 +0000

    Init repo

---
+++ src/components/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>Eat Slower ⏳<br/>Eat Less 📉<br/>7-Day Challenge 🗓</h1>
+            </header>
+            <div className="content">
+                <p>Are you up to the challenge?<br/>
+                Become a slow eater in one week <br/>
+                and reap all the health benefits that come with it!</p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
