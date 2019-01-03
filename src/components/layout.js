161b6085994b047afbdd12d0d728747b3c05abef
Tiago Martinho <t.martinho@icloud.com>
2019-01-03 19:21:52 +0000

    Remove Menu component

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,17 +3,14 @@ import PropTypes from 'prop-types'
 
 import '../assets/scss/main.scss'
 import Header from './Header'
-import Menu from './Menu'
 import Footer from './Footer'
 
 class Layout extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            isMenuVisible: false,
             loading: 'is-loading'
         }
-        this.handleToggleMenu = this.handleToggleMenu.bind(this)
     }
 
     componentDidMount () {
@@ -28,24 +25,15 @@ class Layout extends React.Component {
         }
     }
 
-    handleToggleMenu() {
-        this.setState({
-            isMenuVisible: !this.state.isMenuVisible
-        })
-    }
-
     render() {
         const { children } = this.props
 
         return (
-            <div className={`body ${this.state.loading} ${this.state.isMenuVisible ? 'is-menu-visible' : ''}`}>
                 <div id="wrapper">
-                    <Header onToggleMenu={this.handleToggleMenu} />
+                    <Header/>
                     {children}
                     <Footer />
                 </div>
-                <Menu onToggleMenu={this.handleToggleMenu} />
-            </div>
         )
     }
 }
